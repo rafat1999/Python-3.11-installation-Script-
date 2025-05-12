f79d962db9f5ff14f28097ff0fb8ae67c3cf438f
Shihab Hossen Rafat <100786024+rafat1999@users.noreply.github.com>
2025-05-12 11:47:18 +0600

    Add files via upload

---
+++ Python_3_11_install_script.sh
@@ -0,0 +1,18 @@
+echo "Install Build Dependencies"
+
+sudo apt update
+sudo apt install -y make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev \
+libffi-dev liblzma-dev
+
+echo "Go to /usr/src and download Python 3.11.9"
+cd /usr/src
+sudo wget https://www.python.org/ftp/python/3.11.9/Python-3.11.9.tgz
+sudo tar xzf Python-3.11.9.tgz
+cd Python-3.11.9
+
+echo "Configure and Build"
+sudo ./configure --enable-optimizations
+sudo make -j$(nproc)
+sudo make altinstall
